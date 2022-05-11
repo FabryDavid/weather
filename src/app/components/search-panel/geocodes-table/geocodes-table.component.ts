b63aad598c43c87c613b47838969298b05c182f0
Fábry Dávid <fabry.david@developit.hu>
2022-05-11 10:29:29 +0200

    search panel refactoring

---
+++ src/app/components/search-panel/geocodes-table/geocodes-table.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Geocode} from "../../../classes/geocode/geocode.Class";
+
+@Component({
+  selector: 'app-geocodes-table',
+  templateUrl: './geocodes-table.component.html',
+  styleUrls: ['./geocodes-table.component.scss']
+})
+export class GeocodesTableComponent implements OnInit {
+  @Output() selectItem:EventEmitter<Geocode>=new EventEmitter<Geocode>()
+
+  @Input() geocodes: Array<Geocode> = []
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/search-panel/search-input/search-input.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Geocode} from "../../../classes/geocode/geocode.Class";
+import {faCity} from '@fortawesome/free-solid-svg-icons';
+import {GeocodingService} from "../../../services/geocoding.service";
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.scss']
+})
+export class SearchInputComponent implements OnInit {
+  @Output() geocodingsFound: EventEmitter<Array<Geocode>> = new EventEmitter<Array<Geocode>>()
+
+  faCity = faCity;
+  searchText = 'salgótarján'
+
+  constructor(
+    private geocodingService: GeocodingService,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  searchGeocoding() {
+    const searchText = this.searchText.trim()
+
+    if (!searchText) {
+      return
+    }
+
+    this.geocodingService.getGeocoding(searchText).subscribe((data) => {
+      this.geocodingsFound.emit(data)
+    })
+  }
+}
